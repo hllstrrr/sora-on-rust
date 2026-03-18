6a46dc38b795b8f169af82e0438b3fe9bcf75562
hllstrrr <hllstrrr@gmail.com>
2026-03-18 03:28:19 +0000

    passing state to command, and renaming "BotCommand" to just "Command" because BotCommand looks ugly to read

--- src/commands/cmd.rs
+++ src/commands/cmd.rs
@@ -1,36 +1,39 @@
 use async_trait::async_trait;
 use whatsapp_rust::client::Client;
 use wacore::types::message::MessageInfo;
+use waproto::whatsapp::Message;
 use linkme::distributed_slice;
+use crate::state::AppState;
+use std::sync::Arc;
 
 #[distributed_slice]
-pub static COMMANDS: [&(dyn BotCommand + Sync)] = [..];
+pub static COMMANDS: [&(dyn Command + Sync)] = [..];
 
 #[async_trait]
-pub trait BotCommand: Send + Sync {
+pub trait Command: Send + Sync {
     fn name(&self) -> &str;
     fn aliases(&self) -> &[&str];
     fn category(&self) -> &str;
-    async fn execute(&self, client: &Client, info: &MessageInfo) -> anyhow::Result<()>;
+    async fn execute(&self, client: &Client, msg: &Message, info: &MessageInfo, state: &Arc<AppState>) -> anyhow::Result<()>;
 }
 
 #[macro_export]
 macro_rules! cmd {
-    ($struct_name:ident, name: $name:expr, aliases: [$($alias:expr),*], category: $cat:expr, execute: |$client:ident, $msg:ident, $info:ident| $body:block) => {
+    ($struct_name:ident, name: $name:expr, aliases: [$($alias:expr),*], category: $cat:expr, execute: |$client:ident, $msg:ident, $info:ident, $state: ident| $body:block) => {
         pub struct $struct_name;
 
         #[async_trait::async_trait]
-        impl crate::commands::cmd::BotCommand for $struct_name {
+        impl crate::commands::cmd::Command for $struct_name {
             fn name(&self) -> &str { $name }
             fn aliases(&self) -> &[&str] { &[$($alias),*] }
             fn category(&self) -> &str { $cat }
-            async fn execute(&self, $client: &whatsapp_rust::client::Client, $info: &wacore::types::message::MessageInfo) -> anyhow::Result<()> {
+            async fn execute(&self, $client: &whatsapp_rust::client::Client, $msg: &waproto::whatsapp::Message, $info: &wacore::types::message::MessageInfo, $state: &std::sync::Arc<crate::state::AppState> ) -> anyhow::Result<()> {
                 $body;
                 Ok(())
             }
         }
 
         #[linkme::distributed_slice(crate::commands::cmd::COMMANDS)]
-        static COMMAND: &(dyn crate::commands::cmd::BotCommand + Sync) = &$struct_name;
+        static COMMAND: &(dyn crate::commands::cmd::Command + Sync) = &$struct_name;
     };
 }
