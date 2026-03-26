e2a3b7f4dde2419189e8c54e2511e42f52958be6
hllstrrr <hllstrrr@gmail.com>
2026-03-26 17:59:22 +0000

    fart: bot mode and superuser

--- src/config.rs
+++ src/config.rs
@@ -6,19 +6,20 @@ pub struct AppConfig {
     pub prefixes: Vec<String>,
     pub db_path: String,
     pub custom_code: String,
-
+    pub mode: String,
     #[serde(skip)]
     pub phone_number: String,
+    pub superuser: Option<String>,
 }
 
 impl AppConfig {
     pub fn load() -> anyhow::Result<Self> {
         dotenvy::dotenv().ok();
         let phone = std::env::var("PHONE_NUMBER").expect("PHONE_NUMBER must be set in .env");
-
+        let su = std::env::var("SUPERUSER").ok();
         let toml_str = fs::read_to_string("Config.toml")?;
         let mut config: AppConfig = toml::from_str(&toml_str)?;
-        
+        config.superuser = su;
         config.phone_number = phone;
         Ok(config)
     }
--- src/handler.rs
+++ src/handler.rs
@@ -1,17 +1,31 @@
 use log::{error, info};
 use std::sync::Arc;
 use crate::state::AppState;
-use wacore::types::events::Event;
+use wacore::{client::context::SendContextResolver, types::events::Event};
 use whatsapp_rust::client::Client;
 use crate::commands::cmd::COMMANDS;
 use crate::config::AppConfig;
 use crate::utils::MessageExt;
+use tokio::sync::RwLock;
+use std::sync::LazyLock;
+static SUPERUSER_LID: LazyLock<RwLock<Option<String>>> = LazyLock::new(|| RwLock::new(None));
 pub async fn event_handler(event: Event, client: Arc<Client>, config: Arc<AppConfig>, state: Arc<AppState>) {
     match event {
         Event::Connected(_) => {
             info!("✅ Bot connected successfully!");
+            if let Some(su_pn) = &config.superuser {
+                if let Some(jid) = client.get_lid_for_phone(su_pn).await {
+                    let lid_user = jid.to_string();
+                    
+                    let mut lock = SUPERUSER_LID.write().await;
+                    *lock = Some(lid_user.clone());
+                }
+            }
+            
         }
         Event::Message(msg, info) => {
+            // println!("{:#?}", msg);
+            let start = std::time::Instant::now();
             if let Some(exp) = msg.get_expiration_timer() {
                 state.set_expiration(&info.source.chat.to_string(), exp);
             }
@@ -21,7 +35,24 @@ pub async fn event_handler(event: Event, client: Arc<Client>, config: Arc<AppCon
                 Some(p) => p,
                 None => return,
             };
-
+            if config.mode == "self" {
+                let sender = &info.source.sender.user;
+    
+                let me = info.source.is_from_me;
+                let su = if info.source.sender.is_lid() {
+                    if let Ok(lock) = SUPERUSER_LID.try_read() {
+                        lock.as_deref() == Some(sender.as_str())
+                    } else {
+                        false
+                    }
+                } else {
+                    config.superuser.as_ref() == Some(&sender)
+                };
+                let privileged = me || su;
+                if !privileged {
+                    return;
+                }
+            }
             let msg_arc = Arc::from(msg);
             let info_arc = Arc::new(info);
                 let body = &text[prefix.len()..];
@@ -36,14 +67,16 @@ pub async fn event_handler(event: Event, client: Arc<Client>, config: Arc<AppCon
                             info: Arc::clone(&info_arc),
                             state: Arc::clone(&state),
                         };
-                
+                        println!("Internal: {:?}", start.elapsed());
                         if let Err(e) = cmd.execute(ctx).await {
                             error!("Error executing {}: {}", cmd_name, e);
-                        }
-                        return;
+                        }                        
                     }
                 }
             }
+            
+            let duration = start.elapsed();
+            println!("Executed in {:?}", duration);
         }
         _ => {}
     }
