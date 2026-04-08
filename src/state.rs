48aebe899c4d1d190b5e0dcdd566d9254344806a
hllstrrr <hllstrrr@gmail.com>
2026-04-08 17:55:18 +0000

    feat: tweak http_client for the best performance possible

--- src/state.rs
+++ src/state.rs
@@ -1,4 +1,5 @@
 use dashmap::DashMap;
+use reqwest::header::{ACCEPT, ACCEPT_LANGUAGE, HeaderMap, HeaderValue, USER_AGENT};
 use serde::{Deserialize, Serialize};
 use std::sync::{Arc, RwLock};
 use std::time::Instant;
@@ -34,9 +35,25 @@ pub struct AppState {
 impl AppState {
     pub fn load(config: Arc<AppConfig>) -> Arc<Self> {
         let start_time = Instant::now();
-        let db = sled::Config::new().path("database/chat").cache_capacity(10 * 1024 * 1024).mode(sled::Mode::HighThroughput).open().expect("Errpr opening sled database");
+        let db = sled::Config::new()
+            .path("database/chat")
+            .cache_capacity(10 * 1024 * 1024)
+            .mode(sled::Mode::HighThroughput)
+            .open()
+            .expect("Errpr opening sled database");
         let settings = DashMap::new();
-        let http_client = reqwest::Client::builder().redirect(reqwest::redirect::Policy::default()).cookie_store(true).build().unwrap();
+        let mut headers = HeaderMap::new();
+        headers.insert(USER_AGENT, HeaderValue::from_static("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"));
+        headers.insert(ACCEPT, HeaderValue::from_static("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,video/mp4,*/*;q=0.8"));
+        headers.insert(ACCEPT_LANGUAGE, HeaderValue::from_static("en-US,en;q=0.5"));
+        let http_client = reqwest::Client::builder()
+            .default_headers(headers)
+            .tcp_keepalive(std::time::Duration::from_secs(60))
+            .pool_idle_timeout(std::time::Duration::from_secs(90))
+            .pool_max_idle_per_host(10)
+            .build()
+            .unwrap();
+        // let http_client = reqwest::Client::new();
         // hydration from db to cache
         for (key, value) in db.iter().flatten() {
             let jid = String::from_utf8_lossy(&key).to_string();
@@ -58,13 +75,14 @@ impl AppState {
             prefixes: RwLock::new(config.prefixes.clone()),
             config,
         })
-    }    
+    }
 
     pub fn set_expiration(self: Arc<Self>, jid: String, expiration: u32) {
         if let Some(current) = self.settings.get(&jid)
-            && current.expiration == expiration {
-                return;
-            }
+            && current.expiration == expiration
+        {
+            return;
+        }
         let jid_db = jid.clone();
         self.settings.insert(jid, ChatSettings { expiration });
         let state_clone = Arc::clone(&self);
@@ -102,6 +120,4 @@ impl AppState {
             _ => Err("invalid datatype for this field"),
         }
     }
-
-
 }
